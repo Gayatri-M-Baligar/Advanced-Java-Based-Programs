097ace79de75ee6665a244e81867d8c8135f052a
Gayatri-M-Baligar <153185254+Gayatri-M-Baligar@users.noreply.github.com>
2025-05-21 18:29:55 +0530

    Update ColorSearch.java

--- 1_ListOperations/ColorSearch.java
+++ 1_ListOperations/ColorSearch.java
@@ -1,9 +1,9 @@
+/*Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not*/
+
 package lab_1;
 import java.util.ArrayList;
 public class ColorSearch {
-	
-
-	
 	    public static void main(String[] args) {
 	        // Create an ArrayList of colors
 	        ArrayList<String> colors = new ArrayList<>();
