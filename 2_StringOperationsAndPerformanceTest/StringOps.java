3f9dbfa486e185f7591621152ff8478233859448
Gayatri-M-Baligar <153185254+Gayatri-M-Baligar@users.noreply.github.com>
2025-05-21 18:21:48 +0530

    Update StringOps.java

--- 2_StringOperationsAndPerformanceTest/StringOps.java
+++ 2_StringOperationsAndPerformanceTest/StringOps.java
@@ -1,7 +1,12 @@
-package lab_2;
+/*Develop a java program for performing various string operations with different string
+handling functions directed as follows
+String Creation and Basic Operations, Length and Character Access, String Comparison, String
+Searching, Substring Operations , String Modification, Whitespace Handling, String
+Concatenation, String Splitting, StringBuilder Demo, String Formatting , Validate Email with
+contains( ) and startsWith() and endsWith()*/
 
+package lab_2;
 public class StringOps {
-	
 	    public static void main(String[] args) {
 	        // 1. String Creation and Basic Operations
 	        String str1 = "Hello";
