e5ac8be4215739f9b5122fafe4010ef87b0f44cc
Gayatri-M-Baligar <153185254+Gayatri-M-Baligar@users.noreply.github.com>
2025-05-21 18:11:55 +0530

    Update CapitalizeWords.java

--- 3_StringOperationsSuite/CapitalizeWords.java
+++ 3_StringOperationsSuite/CapitalizeWords.java
@@ -1,3 +1,6 @@
+/*Write a Java Program for Capitalizing the first letter of each word. using user defined function
+capitalizeWords()*/
+
 package lab_3;
 import java.util.Scanner;
 public class CapitalizeWords {
