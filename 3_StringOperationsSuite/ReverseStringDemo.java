e10f87c7d9d4368452313d81806387ddf1fe806f
Gayatri-M-Baligar <153185254+Gayatri-M-Baligar@users.noreply.github.com>
2025-05-21 18:14:26 +0530

    Update ReverseStringDemo.java

--- 3_StringOperationsSuite/ReverseStringDemo.java
+++ 3_StringOperationsSuite/ReverseStringDemo.java
@@ -1,3 +1,6 @@
+/*Write a Java Program for Reversing the characters in a string using user defined function
+reverseString().*/
+
 package lab_3;
 import java.util.Scanner;
 public class ReverseStringDemo {
