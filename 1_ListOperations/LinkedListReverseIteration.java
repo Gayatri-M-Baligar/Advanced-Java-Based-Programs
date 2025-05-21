9c5235d04c5e751c1537859e8ef067782e255ed6
Gayatri-M-Baligar <153185254+Gayatri-M-Baligar@users.noreply.github.com>
2025-05-21 18:24:50 +0530

    Update LinkedListReverseIteration.java

--- 1_ListOperations/LinkedListReverseIteration.java
+++ 1_ListOperations/LinkedListReverseIteration.java
@@ -1,8 +1,10 @@
+/*Write a Java program to iterate a linked list in reverse order (using
+objlist.descendingIterator())*/
+
 package lab_1;
 import java.util.LinkedList;
 import java.util.Iterator;
 public class LinkedListReverseIteration {
-
 	    public static void main(String[] args) {
 	        // Step 1: Create a LinkedList and add elements
 	        LinkedList<String> colors = new LinkedList<>();
