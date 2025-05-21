f6fd855f7bc9dca4898f4fd39c0c7e7255ee6323
Gayatri-M-Baligar <153185254+Gayatri-M-Baligar@users.noreply.github.com>
2025-05-21 18:20:13 +0530

    Update StringOperationsDemo.java

--- 3_StringOperationsSuite/StringOperationsDemo.java
+++ 3_StringOperationsSuite/StringOperationsDemo.java
@@ -1,8 +1,13 @@
+/*Develop a java program for performing various string operations with different string
+handling functions directed as follows:
+String Creation and Basic Operations, Length and Character Access, String Comparison, String
+Searching, Substring Operations , String Modification, Whitespace Handling, String Concatenation,
+String Splitting, StringBuilder Demo, String Formatting , Validate Email with contains( ) and
+startsWith() and endsWith()*/
+
+
 package lab_3;
-
 public class StringOperationsDemo {
-
-
 	    public static void main(String[] args) {
 	        // 1. String Creation and Basic Operations
 	        String str1 = "Hello";
