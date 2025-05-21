c3c1b878640bd96ad8ed042a64a35186cc6bb5fb
Gayatri-M-Baligar <153185254+Gayatri-M-Baligar@users.noreply.github.com>
2025-05-21 18:28:10 +0530

    Update ColorSort.java

--- 1_ListOperations/ColorSort.java
+++ 1_ListOperations/ColorSort.java
@@ -1,9 +1,10 @@
+/*Write a java program for getting different colors through ArrayList interface and sort them
+using Collections.sort( ArrayListObj)*/
+
 package lab_1;
 import java.util.ArrayList;
 import java.util.Collections;
 public class ColorSort {
-	
-
 	    public static void main(String[] args) {
 	        // Step 1: Create an ArrayList of colors
 	        ArrayList<String> colors = new ArrayList<>();
