16d8a3a1ebe22d41d489e32a00f5030eda5dc4ea
Gayatri-M-Baligar <153185254+Gayatri-M-Baligar@users.noreply.github.com>
2025-05-21 18:17:10 +0530

    Update StringTruncator.java

--- 3_StringOperationsSuite/StringTruncator.java
+++ 3_StringOperationsSuite/StringTruncator.java
@@ -1,3 +1,6 @@
+/*Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user
+defined function truncate()*/
+
 package lab_3;
 import java.util.Scanner;
 public class StringTruncator {
